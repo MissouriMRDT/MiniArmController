daf89c2f87a4c58f7d292029d71423136b910828
Brendan Westley <brendan@westley.us>
2024-11-18 12:35:22 -0600

    Superseded changes

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -1,57 +1,98 @@
-#include "AS5600.h"
 #include <Arduino.h>
+#include <SimpleFOC.h>
 
-AS5600 as5600[2] = {AS5600(&Wire1), AS5600(&Wire2)};
+// magnetic sensor instance - I2C
+MagneticSensorI2C sensor1 = MagneticSensorI2C(AS5600_I2C);
+MagneticSensorI2C sensor2 = MagneticSensorI2C(AS5600_I2C);
+
+// BLDC motor & driver instance
+BLDCMotor motor1 = BLDCMotor(8);
+BLDCMotor motor2 = BLDCMotor(8);
+// UH, UL, VH, VL, WH, WL
+BLDCDriver6PWM driver1 = BLDCDriver6PWM(8, 7, 4, 33, 6, 9);
+BLDCDriver6PWM driver2 = BLDCDriver6PWM(36, 37, 29, 28, 2, 3);
+
+Commander command = Commander(Serial);
+float target_angle1 = 0;
+float target_angle2 = 0;
+void setTargetA(char *cmd) { command.scalar(&target_angle1, cmd); }
+void setTargetB(char *cmd) { command.scalar(&target_angle2, cmd); }
+void toggleEnabled(char *_) {
+  if (motor1.enabled)
+    motor1.disable();
+  else
+    motor1.enable();
+  if (motor2.enabled)
+    motor2.disable();
+  else
+    motor2.enable();
+}
+void reset(char *_) { SCB_AIRCR = 0x05FA0004; }
 
 void setup() {
   Serial.begin(115200);
-  Wire1.begin();
-  Wire2.begin();
-  for (int i = 0; i < 2; i++) {
-    as5600[i].begin(); // Software direction control disabled. Set to clockwise
-                       // in hardware.
-    Serial.print("Connected: ");
-    Serial.println(as5600[i].isConnected());
+  SimpleFOCDebug::enable(&Serial);
+
+  delay(2000); // Wait for serial.
+  Serial.println("Starting...");
+
+  // Initialize encoder sensor hardware.
+  sensor1.init(&Wire1);
+  sensor2.init(&Wire2);
+  // Link the motors to the sensors.
+  motor1.linkSensor(&sensor1);
+  motor2.linkSensor(&sensor2);
+
+  driver1.voltage_power_supply = 5; // IMPORTANT!
+  if (!driver1.init()) {
+    Serial.println("driver1.init failed.");
   }
-}
 
-const uint32_t OUTPUT_INTERVAL = 500; // ms
-uint32_t lastOutput = 0;
-double deltaX = 0, deltaY = 0;
-uint8_t moveX = 0, moveY = 0;
-int32_t currentX = 0, currentY = 0, lastX = 0, lastY = 0;
-double sensitivity = 5;
+  driver2.voltage_power_supply = 5; // IMPORTANT!
+  if (!driver2.init()) {
+    Serial.println("driver2.init failed.");
+  }
 
-int8_t clamp(double x) {
-  if (x >= INT8_MAX)
-    return INT8_MAX;
-  if (x <= INT8_MIN)
-    return INT8_MIN;
-  return x;
+  // Link drivers.
+  motor1.linkDriver(&driver1);
+  motor2.linkDriver(&driver2);
+
+  // Aligning voltage.
+  motor1.voltage_sensor_align = 3;
+  motor2.voltage_sensor_align = 3;
+
+  // Set motion control loop to be used.
+  motor1.controller = MotionControlType::angle;
+  motor2.controller = MotionControlType::angle;
+
+  motor1.useMonitoring(Serial);
+  motor2.useMonitoring(Serial);
+
+  // Initialize motor.
+  motor1.init();
+  motor2.init();
+  // Align sensor and start FOC.
+  motor1.initFOC();
+  motor2.initFOC();
+
+  // Add serial commands.
+  command.add('a', setTargetA, "target angle A");
+  command.add('b', setTargetB, "target angle B");
+  command.add('t', toggleEnabled, "toggle enabled");
+  command.add('r', reset, "toggle enabled");
+
+  Serial.println(F("System ready.\nAvailable commands:\n  a<angle>: set motor "
+                   "1 target angle.\n  b<angle>: set "
+                   "motor 2 target angle.\n  t: toggle enabled.\n  r: reset."));
+
+  delay(1000);
 }
 
 void loop() {
-  currentX = as5600[0].getCumulativePosition();
-  currentY = as5600[1].getCumulativePosition();
-  deltaX += (currentX - lastX) / sensitivity;
-  deltaY += (currentY - lastY) / sensitivity;
-  if (deltaX < 1 || deltaX > 1 || deltaY < 1 || deltaY > 1) {
-    int8_t moveX = clamp(deltaX);
-    int8_t moveY = clamp(deltaY);
-    Mouse.move(moveX, moveY);
-    deltaX -= moveX;
-    deltaY -= moveY;
-  }
-  lastX = currentX;
-  lastY = currentY;
+  motor1.loopFOC();
+  motor2.loopFOC();
+  motor1.move(target_angle1);
+  motor2.move(target_angle2);
 
-  if (millis() - lastOutput >= OUTPUT_INTERVAL) {
-    lastOutput = millis();
-    Serial.print('0'); // Scale ardino serial plotter minimum
-    for (int i = 0; i < 2; i++) {
-      Serial.print('\t');
-      Serial.print(as5600[i].getCumulativePosition());
-    }
-    Serial.println();
-  }
+  command.run();
 }
